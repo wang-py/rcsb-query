1915959bda15c01034ac7ca7dbd18fecc06a4de7
Panyue Wang <pywang@ucdavis.edu>
2024-05-21 11:57:42 -0700

    attribute search

--- rcsb_search.py
+++ rcsb_search.py
@@ -3,13 +3,15 @@ from rcsbsearchapi import rcsb_attributes as attrs
 from rcsbsearchapi.const import STRUCTURE_ATTRIBUTE_SEARCH_SERVICE
 
 q1 = TextQuery("Respiratory Complex I")
-q2 = AttributeQuery("exptl.method", "exact_match", "electron microscopy",
+q2 = AttributeQuery("rcsb_polymer_instance_annotation.annotation_lineage.name",
+                    "contains_words", "NADH oxidoreductase",
                     STRUCTURE_ATTRIBUTE_SEARCH_SERVICE)
-q3 = attrs.rcsb_accession_info.deposit_date > "2020-01-01"
+q3 = attrs.rcsb_accession_info.deposit_date > "2010-01-01"
 
 query = q1 & q2 & q3
 
-# for assemblyid in query("assembly"):
-#     print(assemblyid)
+hits = [x for x in list(query(results_verbosity="minimal")) if x['score'] > 0.8]
 
-print(query.count())
+print("total number of results: ", query.count())
+print(f"scores above 0.8: {len(hits)}")
+print("last 10 hits: ", hits[-10:])
