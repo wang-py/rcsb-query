d5d2584796524163ef1fe96b72b82f9d6cd87153
Panyue Wang <pywang@ucdavis.edu>
2024-05-23 15:49:43 -0700

    search structures with water

--- rcsb_search.py
+++ rcsb_search.py
@@ -3,12 +3,14 @@ from rcsbsearchapi import rcsb_attributes as attrs
 from rcsbsearchapi.const import STRUCTURE_ATTRIBUTE_SEARCH_SERVICE
 
 q1 = TextQuery("Respiratory Complex I")
-q2 = AttributeQuery("rcsb_polymer_instance_annotation.annotation_lineage.name",
-                    "contains_words", "NADH oxidoreductase",
+q2 = AttributeQuery("rcsb_polymer_entity.rcsb_macromolecular_names_combined.name",
+                    "contains_phrase", "Complex I",
                     STRUCTURE_ATTRIBUTE_SEARCH_SERVICE)
-q3 = attrs.rcsb_accession_info.deposit_date > "2010-01-01"
-
-query = q1 & q2 & q3
+q3 = AttributeQuery("rcsb_entry_info.deposited_solvent_atom_count",
+                    "greater", 0,
+                    STRUCTURE_ATTRIBUTE_SEARCH_SERVICE)
+q4 = attrs.rcsb_accession_info.deposit_date > "2010-01-01"
+query = q1 & q2 & q3 & q4
 
 hits = [x for x in list(query(results_verbosity="minimal"))
         if x['score'] > 0.8]
