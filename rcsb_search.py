deb2820ca7cee7ef7840549a6ca08de8cde9f696
Panyue Wang <pywang@ucdavis.edu>
2024-05-24 10:16:18 -0700

    only searches complex I

--- rcsb_search.py
+++ rcsb_search.py
@@ -10,11 +10,11 @@ q3 = AttributeQuery("rcsb_entry_info.deposited_solvent_atom_count",
                     "greater", 0,
                     STRUCTURE_ATTRIBUTE_SEARCH_SERVICE)
 q4 = attrs.rcsb_accession_info.deposit_date > "2010-01-01"
-query = q1 & q2 & q3 & q4
+query = q1 & q2 & q3  # & q4
 
 hits = [x for x in list(query(results_verbosity="minimal"))
         if x['score'] > 0.8]
 
 print("total number of results: ", query.count())
 print(f"scores above 0.8: {len(hits)}")
-print("last 10 hits: ", hits[-10:])
+print("hits:\n", hits)
