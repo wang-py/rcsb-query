31212980ef17254dea2d1808ac908491db73a8c4
Panyue Wang <pywang@ucdavis.edu>
2024-06-03 16:40:14 -0700

    download structures

--- rcsb_search.py
+++ rcsb_search.py
@@ -1,9 +1,20 @@
 from rcsbsearchapi.search import TextQuery, AttributeQuery
 from rcsbsearchapi import rcsb_attributes as attrs
 from rcsbsearchapi.const import STRUCTURE_ATTRIBUTE_SEARCH_SERVICE
+from urllib.request import urlretrieve
+
+
+def download_pdb(pdb_identifier):
+    pdb_filename = pdb_identifier + ".pdb"
+    url = (
+            "https://files.rcsb.org/download/" + pdb_filename
+            )
+    urlretrieve(url, pdb_filename)
+
 
 q1 = TextQuery("Respiratory Complex I")
-q2 = AttributeQuery("rcsb_polymer_entity.rcsb_macromolecular_names_combined.name",
+q2 = AttributeQuery("rcsb_polymer_entity" +
+                    ".rcsb_macromolecular_names_combined.name",
                     "contains_phrase", "Complex I",
                     STRUCTURE_ATTRIBUTE_SEARCH_SERVICE)
 q3 = AttributeQuery("rcsb_entry_info.deposited_solvent_atom_count",
@@ -14,7 +25,10 @@ query = q1 & q2 & q3  # & q4
 
 hits = [x for x in list(query(results_verbosity="minimal"))
         if x['score'] > 0.8]
-
+hits_identifiers = [x['identifier'] for x in hits]
 print("total number of results: ", query.count())
 print(f"scores above 0.8: {len(hits)}")
-print("hits:\n", hits)
+print("hits:\n", hits_identifiers)
+
+for hit in hits_identifiers:
+    download_pdb(hit)
